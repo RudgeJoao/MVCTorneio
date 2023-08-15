c7ccb6e3a0585254ecbfd11821225a6355e1b642
Joao Rudge <joao.rudge@t-systems.com>
2023-08-15 16:26:04 -0300

    Estabelecendo conexão com o banco oracle e CRUD funcionando

---
+++ MicrosoftMVC/Data/MicrosoftMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MicrosoftMVC.Models;
+
+namespace MicrosoftMVC.Data
+{
+    public class MicrosoftMVCContext : DbContext
+    {
+        public MicrosoftMVCContext (DbContextOptions<MicrosoftMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MicrosoftMVC.Models.Lutador> Lutador { get; set; } = default!;
+    }
+}
---
+++ MicrosoftMVC/Migrations/20230815132247_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MicrosoftMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lutador",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    ArtesMarciais = table.Column<int>(type: "int", nullable: false),
+                    TotalLutas = table.Column<int>(type: "int", nullable: false),
+                    Derrotas = table.Column<int>(type: "int", nullable: false),
+                    Vitorias = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lutador", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lutador");
+        }
+    }
+}
